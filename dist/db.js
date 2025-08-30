3f73c65510707f1f4a1f6bb1856dd3f695554c77
prahumansingh22 <pradhuman362@gmail.com>
2025-08-30 23:57:50 +0530

    Apply .gitignore

--- dist/db.js
+++
@@ -1,6 +0,0 @@
-var _a;
-import { PrismaClient } from "../prisma/app/generated/prisma/index.js";
-const globalForPrisma = global;
-export const prisma = (_a = globalForPrisma.prisma) !== null && _a !== void 0 ? _a : new PrismaClient();
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prisma;
